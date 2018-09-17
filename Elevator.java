ce4062717909befbd1d2c5e69a2bb0f18b82951d
Simon Su <31625767+xmansimon@users.noreply.github.com>
2018-09-17 17:43:54 -0400

    Class Setup
    Class Setup

---
+++ Elevator.java
@@ -0,0 +1,16 @@
+package cscie55.hw1.elevator;
+
+/**
+ *  Get the sum of two integers.
+ *  @param floorNumber number of floors in the building, and set it to 7.
+ *  @param  currentFloor a field for tracking the Elevator's current floor .
+ *  @param  direction a field for tracking the Elevator's direction of travel.
+ *  @param  numPeople the number of passengers destined for that floor.
+ *  
+ */
+public class Elevator{
+    private static final int floorNumber = 7;
+    private static int currentFloor;
+    private static boolean direction; // false go up, true go down
+    private static int numPeople []; 
+}
