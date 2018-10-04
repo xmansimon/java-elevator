f67fd3ed308a3fbf7e57ce1a37533fb1a97aa1b7
Simon Su <31625767+xmansimon@users.noreply.github.com>
2018-10-04 00:41:23 -0400

    Update

--- elevator/Elevator.java
+++ elevator/Elevator.java
@@ -10,40 +10,55 @@ package cscie55.hw1.elevator;
  */
 public class Elevator{
     private static final int floorNumber = 7;
-    private static int currentFloor; // elevator starts from 1st floor
-    private static boolean direction; // false go up, true go down
-    private static int[][] destination = new int[8][2]; 
+    private static final int capacity = 10;
+    private  int currentFloor; // elevator starts from 1st floor
+    private  boolean direction; // false go up, true go down
+    private  int[][] destination = new int[8][2]; 
 
-
+    private int passengerInElevator; //total number of passenger in the elvator
 /**
 * Constructor
 */
-    public Elevator(){
+    public Elevator(Building building){
     }
 
 /**
 * move method
 */
     public void move(){
-       if(currentFloor == floorNumber){ // change direction if elevator reaches top
-           direction = true;  
-       }else if(currentFloor == 1){ // change direction if elevator reaches 1st floor
-           direction = false; 
-       }
         if(direction == false){ // go up 
             currentFloor++;
         }else{
             currentFloor--;
        }
-       destination[currentFloor][1] = 0; // clears number of people destined for the current floor. 
-       System.out.println(toString());
+        if(currentFloor == floorNumber){ // change direction if elevator reaches top
+           direction = true;  
+        }else if(currentFloor == 1){ // change direction if elevator reaches 1st floor
+           direction = false; 
+       }
+        destination[currentFloor][1] = 0; // clears number of people destined for the current floor. 
+        System.out.println(toString());
     }
 /**
 * @param destinationFloor Passenger destination floor 
 */
-    public void boardPassenger(int destinationFloor){
-        destination[destinationFloor][0] = destinationFloor;  // set the first column to be the destined floor.
-        destination[destinationFloor][1]++;  // set second column as number of people destined to the floor. 
+    public void boardPassenger(int destinationFloor) throws ElevatorFullException{
+        try{
+            destination[destinationFloor][0] = destinationFloor;  // set the first column to be the destined floor.
+            destination[destinationFloor][1]++;  // set second column as number of people destined to the floor. 
+            passengerInElevator = 0; //reset total passenger variable
+            for(int i =0; i < destination.length; i++){
+                passengerInElevator = destination[i][1] + passengerInElevator; // 1st column ist the number of people
+            }
+    
+ 
+        }catch(ElevatorFullException efe){
+            if(passengerInElevator = capacity){
+                throw new ElevatorFullException("Elevator reaches full capacity", efe.getCause());
+            }
+
+        }
+       
     }
 
     public String toString(){
@@ -55,7 +70,13 @@ public class Elevator{
     }
 
 
+    public int getCurrentFloor(){
+        return this.currentFloor;
+    }
 
+    public int getPassengers(){
+        return passengerInElevator;
+    }
 
 
 }
